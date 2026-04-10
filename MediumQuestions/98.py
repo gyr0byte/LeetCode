9b9b769a160f469b85d76b7b81502d2434fe441b
Gauravlovescode <gqurav69@gmail.com>
2026-04-10 21:58:08 +0545

    Implement isValidBST function to validate if a binary tree is a binary search tree

---
+++ MediumQuestions/98.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isValidBST(self, root, min_val=float('-inf'), max_val=float('inf')):
+        if root is None:
+            return True
+        if root.val <= min_val or root.val >= max_val:
+            return False
+        return self.isValidBST(root.left, min_val, root.val) and self.isValidBST(root.right, root.val, max_val)
