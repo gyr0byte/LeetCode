f43855ca7acd33ee60ce08e10a3f7d6e60157e36
Gauravlovescode <gqurav69@gmail.com>
2026-04-19 23:39:40 +0545

    Add scoreOfString function to calculate score based on character differences

---
+++ EasyQuestions/3110.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        result = 0
+        for i in range(1, len(s)):
+            result += abs(ord(s[i - 1]) - ord(s[i]))
+        return result
