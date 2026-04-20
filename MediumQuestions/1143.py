ce355385a9925dd8a45c59584633f7a7740ad596
Gauravlovescode <gqurav69@gmail.com>
2026-04-20 09:37:20 +0545

    Refactor longestCommonSubsequence to use dynamic programming instead of recursion

--- MediumQuestions/1143.py
+++ MediumQuestions/1143.py
@@ -1,15 +1,12 @@
 class Solution(object):
     def longestCommonSubsequence(self, text1, text2):
-        memo = {}
-        def recurse(idx1 = 0, idx2 = 0):
-            key = (idx1, idx2)
-            if key in memo:
-                return memo[key]
-            if idx1 == len(text1) or idx2 == len(text2):
-                memo[key] = 0
-            elif text1[idx1] == text2[idx2]:
-                memo[key] = 1 + recurse(idx1 + 1, idx2 + 1)
-            else:
-                memo[key] = max(recurse(idx1 + 1, idx2), recurse(idx1, idx2 + 1))
-            return memo[key]
-        return recurse(0,0)
+        n1, n2 = len(text1), len(text2)
+        table = [[0 for x in range(n2+1)] for x in range(n1+1)]
+        for idx1 in range(n1):
+            for idx2 in range(n2):
+                if text1[idx1] == text2[idx2]:
+                    table[idx1+1][idx2+1] = 1 + table[idx1][idx2]
+                else:
+                    table[idx1+1][idx2+1] = max(table[idx1][idx2+1], table[idx1+1][idx2])
+
+        return table[-1][-1]
