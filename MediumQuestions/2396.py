7ca64c1a251bc7d4dd819c19f455ccb60a72e3d1
gyr0byte <gqurav69@gmail.com>
2026-04-25 13:20:28 +0545

    Add comments to clarify the isStrictlyPalindromic method's logic and return conditions

--- MediumQuestions/2396.py
+++ MediumQuestions/2396.py
@@ -13,3 +13,8 @@ class Solution(object):
         if n >= 4:
             return False
         return (base2 == base2[::-1]) and (base3 == base3[::-1])
+    
+    # you can just return False for n >= 4, 
+    # since the problem states that n is strictly palindromic 
+    # if it is palindromic in all bases from 2 to n - 2, a
+    # nd for n >= 4, it will not be palindromic in base 2.
