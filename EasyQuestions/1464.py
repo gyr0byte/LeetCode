ba1c0808573e432666fadacb2301fa55117856b3
gyr0byte <gqurav69@gmail.com>
2026-05-09 18:38:15 +0545

    Implement maxProduct method to calculate the maximum product of two elements in the array

---
+++ EasyQuestions/1464.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a,b = 0,0
+        for n in nums:
+            if n > a:
+                b = a
+                a = n
+            elif n > b:
+                b = n
+        return (a-1) * (b-1)
