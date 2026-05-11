697c5b33d03934d875ec1b2f675993e8a2f91b54
gyr0byte <gqurav69@gmail.com>
2026-05-11 20:40:05 +0545

    Remove unnecessary docstring from runningSum method in 1480.py

--- EasyQuestions/1480.py
+++ EasyQuestions/1480.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         sum = []
         sum.append(nums[0])
         for i in range(1, len(nums)):
